bd6b0ce49c83314d867035a8b44a2f4c2db17cab
AaminaShaik <152490512+AaminaShaik@users.noreply.github.com>
2026-08-03 21:04:14 +0530

    Fix subnet resource to reference correct VPC ID

--- main.tf
+++ main.tf
@@ -8,7 +8,7 @@ resource "aws_vpc" "vpc" {
 
 
 resource "aws_subnet" "name2" {
-    vpc_id = aws_vpc.name.id
+    vpc_id = aws_vpc.vpc.id
     cidr_block = "10.0.0.0/24"
     tags = {
       Name = "mysubnet1"
